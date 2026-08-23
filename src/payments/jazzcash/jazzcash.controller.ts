ad5ba69e114d5d4fb1b35a5a7555c2613c78f76e
Mir Younas <miryounas8@ohoolabs.com>
2026-08-24 03:10:56 +0500

    refactor payment provider flow

--- src/payments/jazzcash/jazzcash.controller.ts
+++ src/payments/jazzcash/jazzcash.controller.ts
@@ -1,16 +1,17 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import type { Response } from 'express';
-
-import { PaymentsService } from '../payments.service';
 import type { JazzCashCallback } from './jazzcash.types';
+import { JazzcashService } from './jazzcash.service';
 
 @Controller('payments/jazzcash')
 export class JazzcashController {
-  constructor(private readonly paymentsService: PaymentsService) {}
+  constructor(
+    private readonly jazzcashService: JazzcashService
+  ) {}
 
   @Post('callback')
   async callback(@Body() body: JazzCashCallback, @Res() res: Response) {
-    const result = await this.paymentsService.handleJazzCashCallback(body);
+    const result = await this.jazzcashService.handleJazzCashCallback(body);
 
     return res.redirect(result.redirectUrl);
   }
